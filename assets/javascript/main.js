04bf30207783d044d9b2c6c302447fcfe85c1d96
Paraprax <26296581+Paraprax@users.noreply.github.com>
2022-02-08 14:55:35 -0500

    Add onclick property && image path change function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,8 @@
-const doorImage1 = document.getElementById("door1");
+let doorImage1 = document.getElementById("door1");
+const dudDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
+
+const door1Click = () => {
+  doorImage1.src = dudDoorPath;
+};
+
+doorImage1.onclick = door1Click;
