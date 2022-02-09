de060a4e1e7eaf98ec8420cf13fdd1d103dec65c
Paraprax <26296581+Paraprax@users.noreply.github.com>
2022-02-08 19:08:20 -0500

    Add doorRandomizer function in main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,21 +1,25 @@
 let doorImg1 = document.getElementById("door1");
 let doorImg2 = document.getElementById("door2");
 let doorImg3 = document.getElementById("door3");
+let numOfClosedDoors = 3;
 const dudDoorPath = "assets/images/wrong-door.png";
 const qDoorPath = "assets/images/right-door.png";
 
 const door1Click = () => {
   doorImg1.src = dudDoorPath;
 };
-
 const door2Click = () => {
   doorImg2.src = qDoorPath;
 };
-
 const door3Click = () => {
   doorImg3.src = qDoorPath;
 };
 
+const doorRandomizer = () => {
+  let wrongDoor = Math.floor(Math.random() * numOfClosedDoors);
+};
+
+//click events:
 doorImg1.onclick = door1Click;
 doorImg2.onclick = door2Click;
 doorImg3.onclick = door3Click;
