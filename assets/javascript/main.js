56730509e67e52be353400c108e690d3848450ba
Paraprax <26296581+Paraprax@users.noreply.github.com>
2022-02-08 20:27:43 -0500

    Wrap doorClick function logic in isOpen conditionals

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,23 +10,30 @@ const dudDoorPath = "assets/images/wrong-door.png";
 const qDoorPath = "assets/images/right-door.png";
 
 const door1Click = () => {
-  doorImg1.src = openDoor1;
-  doorCounter();
+  if (!isOpen) {
+    doorImg1.src = openDoor1;
+    doorCounter();
+  }
 };
 const door2Click = () => {
-  doorImg2.src = openDoor2;
-  doorCounter();
+  if (!isOpen) {
+    doorImg2.src = openDoor2;
+    doorCounter();
+  }
 };
 const door3Click = () => {
-  doorImg3.src = openDoor3;
-  doorCounter();
+  if (!isOpen) {
+    doorImg3.src = openDoor3;
+    doorCounter();
+  }
 };
 
 const isOpen = (door) => {
   //if the door's src image is currently the closed door:
   if (door.src == closedDoorPath) {
     return false;
-  } else { //if it's any other image:
+  } else {
+    //if it's any other image:
     return true;
   }
 };
